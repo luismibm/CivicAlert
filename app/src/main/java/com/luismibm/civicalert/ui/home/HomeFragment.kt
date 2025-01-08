0026263c91185b630e58a8afc31ff8e4cedc45e7
luismibm <luibenmor@gmail.com>
2025-01-08 15:45:22 +0100

    4.1.2 PT1

--- app/src/main/java/com/luismibm/civicalert/ui/home/HomeFragment.kt
+++ app/src/main/java/com/luismibm/civicalert/ui/home/HomeFragment.kt
@@ -1,10 +1,16 @@
 package com.luismibm.civicalert.ui.home
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.luismibm.civicalert.databinding.FragmentHomeBinding
@@ -12,10 +18,9 @@ import com.luismibm.civicalert.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,7 +37,37 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        locationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            result ->
+            val fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)
+            val coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false)
+            if (fineLocationGranted) {
+                getLocation()
+            } else if (coarseLocationGranted) {
+                getLocation()
+            } else {
+                Toast.makeText(requireContext(), "No Permissions Granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.buttonLocation.setOnClickListener { getLocation() }
+
         return root
+    }
+
+    private fun getLocation() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(requireContext(), "Request Permissions", Toast.LENGTH_SHORT).show()
+            locationPermissionRequest.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        } else {
+            Toast.makeText(requireContext(), "getLocation() Permissions Granted", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {

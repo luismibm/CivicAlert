953156b97a7b1d9491e70cb399bce6e49ab2a5af
luismibm <luibenmor@gmail.com>
2025-01-15 13:46:07 +0100

    4.1.4

--- app/src/main/java/com/luismibm/civicalert/ui/home/HomeFragment.kt
+++ app/src/main/java/com/luismibm/civicalert/ui/home/HomeFragment.kt
@@ -13,6 +13,7 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
@@ -21,7 +22,11 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.luismibm.civicalert.databinding.FragmentHomeBinding
 import java.io.IOException
 import java.util.Locale
@@ -34,6 +39,9 @@ class HomeFragment : Fragment() {
 
     private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    private var mTrackingLocation = false
+    private lateinit var mLocationCallback: LocationCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,21 +65,38 @@ class HomeFragment : Fragment() {
             val fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)
             val coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false)
             if (fineLocationGranted) {
-                getLocation()
+                startTrackingLocation()
             } else if (coarseLocationGranted) {
-                getLocation()
+                startTrackingLocation()
             } else {
                 Toast.makeText(requireContext(), "No Permissions Granted", Toast.LENGTH_SHORT).show()
             }
         }
 
-        binding.buttonLocation.setOnClickListener { getLocation() }
+        mLocationCallback = object : LocationCallback() {
+            @SuppressLint("SetTextI18n")
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult.lastLocation != null) {
+                    fetchAddress(locationResult.lastLocation!!)
+                } else {
+                    binding.textHome.setText("No location known")
+                }
+            }
+        }
+        binding.buttonLocation.setOnClickListener {
+            Toast.makeText(requireContext(), "Clicked: GET LOCATION", Toast.LENGTH_SHORT).show()
+            if (!mTrackingLocation) {
+                startTrackingLocation()
+            } else {
+                stopTrackingLocation()
+            }
+        }
         return root
 
     }
 
     @SuppressLint("DefaultLocale", "SetTextI18n")
-    private fun getLocation() {
+    private fun startTrackingLocation() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(requireContext(), "Request Permissions", Toast.LENGTH_SHORT).show()
             locationPermissionRequest.launch(
@@ -80,18 +105,38 @@ class HomeFragment : Fragment() {
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 )
             )
+        } else {
+            Toast.makeText(requireContext(), "startTrackingLocation() permissions granted", Toast.LENGTH_SHORT).show()
+            mFusedLocationClient.requestLocationUpdates(getLocationRequest(), mLocationCallback, null)
         }
-        mFusedLocationClient.lastLocation.addOnSuccessListener { location ->
-            if (location != null) {
-                fetchAdress(location)
-            } else {
-                binding.textHome.setText("No Location Known")
+        binding.textHome.setText("Loading...")
+        binding.progressBar.setVisibility(ProgressBar.VISIBLE)
+        mTrackingLocation = true
+        binding.buttonLocation.setText("Stop Tracking Location")
+    }
+
+    private fun getLocationRequest(): LocationRequest {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000)
+            .setMinUpdateIntervalMillis(5000)
+            .build()
+        return locationRequest
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun stopTrackingLocation() {
+        if (mTrackingLocation) {
+            binding.apply {
+                progressBar.visibility = ProgressBar.INVISIBLE
+                textHome.setText("Start Tracking Location")
             }
+            mTrackingLocation = false
+            binding.buttonLocation.setText("Start Tracking Location")
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback)
         }
     }
 
     @SuppressLint("SetTextI18n")
-    private fun fetchAdress(location: Location) {
+    private fun fetchAddress(location: Location) {
         val executor = Executors.newSingleThreadExecutor()
         val handler = Handler(Looper.getMainLooper())
         val geocoder = Geocoder(requireContext(), Locale.getDefault())
@@ -109,7 +154,6 @@ class HomeFragment : Fragment() {
                         resultMessage = "No Address Found"
                     }
                 } else {
-
                     val address= addresses[0]
                     val addressParts: ArrayList<String> = ArrayList()
                     for (i in 0..address.maxAddressLineIndex) {
@@ -117,10 +161,11 @@ class HomeFragment : Fragment() {
                     }
                     resultMessage = TextUtils.join("\n", addressParts)
                     var finalResultMessage = resultMessage
-
                     handler.post {
-                        val time = java.text.DateFormat.getTimeInstance().format(System.currentTimeMillis())
-                        binding.textHome.text = "Location: $finalResultMessage \n Time: $time"
+                        if (mTrackingLocation) {
+                            val time = java.text.DateFormat.getTimeInstance().format(System.currentTimeMillis())
+                            binding.textHome.text = "Location: $finalResultMessage \n Time: $time"
+                        }
                     }
                 }
             } catch (e: IOException) {
